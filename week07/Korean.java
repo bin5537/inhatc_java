4499d4c63c8b6b1ded36276d2a46ac47237973e9
Park Subin <subinp5537@gmail.com>
2025-04-17 18:47:56 +0900

    Create Korean.java

---
+++ week07/Korean.java
@@ -0,0 +1,14 @@
+package week07;
+
+public class Korean {
+	String nation = "대한민국";
+	String name;
+	String ssn;
+	String phoneNum;
+	
+	Korean(String name, String ssn, String phoneNum) {
+		this.name = name;
+		this.ssn = ssn;
+		this.phoneNum = phoneNum;
+	}
+}
