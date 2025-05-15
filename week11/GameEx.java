382cb84cd132ec551a01d149848c7b1628eecda8
Park Subin <subinp5537@gmail.com>
2025-05-15 19:35:22 +0900

    Update GameEx.java

--- week11/GameEx.java
+++ week11/GameEx.java
@@ -9,8 +9,17 @@ public class GameEx {
 		game[2] = new SuperMario("슈퍼마리오", "15.1");
 		
 		for (int i=0; i<game.length; i++) {
+			System.out.println("");
 			game[i].start();
 			game[i].gameDesc();
+		}
+
+		System.out.println("\n-- [향상된 for문 사용] --\n");
+		
+		// 향상된 for문 사용
+		for (Game g : game) {
+			g.start();
+			g.gameDesc();
 			System.out.println("");
 		}
 	}
