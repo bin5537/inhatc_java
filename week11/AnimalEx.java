227ffbc242ed997b388d94df3aa2a4a5bbdb5d42
Park Subin <subinp5537@gmail.com>
2025-05-15 18:58:57 +0900

    Update AnimalEx.java

--- week11/AnimalEx.java
+++ week11/AnimalEx.java
@@ -2,10 +2,20 @@ package week11;
 
 public class AnimalEx {
 	public static void main(String[] args) {
+		// 추상 클래스는 객체 생성이 불가능
+		// 대신, 상속은 가능
+		// Animal animal = new Animal();
 		Dog dog = new Dog();
 		Cat cat = new Cat();
 		
 		dog.sound();
 		cat.sound();
+
+		System.out.println("\n-- [자동 타입 변환] --\n");
+		
+		Animal animal = dog;
+		animal.sound();
+		animal = cat;
+		animal.sound();
 	}
 }
