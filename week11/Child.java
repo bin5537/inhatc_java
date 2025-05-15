7b5f5c060cff869d6d9eab53f5f37b49fc52d42e
Park Subin <subinp5537@gmail.com>
2025-05-15 17:38:33 +0900

    Create Child.java

---
+++ week11/Child.java
@@ -0,0 +1,12 @@
+package week11;
+
+public class Child extends Parent {
+	@Override
+	public void method2() {
+		System.out.println("Child-Method2()");
+	}
+	
+	public void method3() {
+		System.out.println("Child-Method3()");
+	}
+}
