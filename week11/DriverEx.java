c073ea5e06a01d26bc2e8ede0490fb71b887ba04
Park Subin <subinp5537@gmail.com>
2025-05-15 18:30:19 +0900

    Update DriverEx.java

--- week11/DriverEx.java
+++ week11/DriverEx.java
@@ -4,6 +4,8 @@ public class DriverEx {
 	public static void main(String[] args) {
 		Driver driver = new Driver();
 		Vehicle v = new Vehicle();
-		
+		driver.drive(v);
+		driver.drive(new Bus());
+		driver.drive(new Taxi());
 	}
 }
